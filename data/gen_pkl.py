ab55d7a4fea9217e05da49429b7d56f025521628
Sinon <yz01@mail.ustc.edu.cn>
2023-03-02 21:59:50 +0800

    Normalize dataset paths

--- data/gen_pkl.py
+++ data/gen_pkl.py
@@ -3,12 +3,12 @@ import glob
 from tqdm import tqdm
 import cv2
 import pickle as pkl
+from config import DATASET_PATH, IMAGE_PATH, DATASET_NAME
 
-
-image_path = '/Users/tal/Documents/Tal/myWork/公式识别/code/WAP/data/off_image_train'
-image_out = 'train_image.pkl'
-laebl_path = 'train_hyb'
-label_out = 'train_label.pkl'
+image_path = os.path.join(DATASET_PATH, IMAGE_PATH[DATASET_NAME])
+image_out = os.path.join(DATASET_PATH, 'train_image.pkl')
+label_path = os.path.join(DATASET_PATH, 'train_hyb')
+label_out = os.path.join(DATASET_PATH, 'train_label.pkl')
 
 images = glob.glob(os.path.join(image_path, '*.bmp'))
 image_dict = {}
@@ -17,18 +17,18 @@ for item in tqdm(images):
 
     img = cv2.imread(item)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    image_dict[os.path.basename(item).replace('_0.bmp','')] = img
+    image_dict[os.path.basename(item).replace('_0.bmp', '')] = img
 
-with open(image_out,'wb') as f:
+with open(image_out, 'wb') as f:
     pkl.dump(image_dict, f)
 
-labels = glob.glob(os.path.join(laebl_path, '*.txt'))
+labels = glob.glob(os.path.join(label_path, '*.txt'))
 label_dict = {}
 
 for item in tqdm(labels):
     with open(item) as f:
         lines = f.readlines()
-    label_dict[os.path.basename(item).replace('.txt','')] = lines
+    label_dict[os.path.basename(item).replace('.txt', '')] = lines
 
-with open(label_out,'wb') as f:
+with open(label_out, 'wb') as f:
     pkl.dump(label_dict, f)
--- data/gen_symbols_struct_dict.py
+++ data/gen_symbols_struct_dict.py
@@ -1,14 +1,16 @@
 import os
 import glob
 from tqdm import tqdm
+from config import DATASET_PATH
 
-label_path = 'test-bak'
+label_path = os.path.join(DATASET_PATH, 'train_hyb')
+word_path = os.path.join(DATASET_PATH, 'word.txt')
 
 labels = glob.glob(os.path.join(label_path, '*.txt'))
 
 words_dict = set(['<eos>', '<sos>', 'struct'])
 
-with open('word.txt', 'w') as writer:
+with open(word_path, 'w') as writer:
     writer.write('<eos>\n<sos>\nstruct\n')
     i = 3
     for item in tqdm(labels):
@@ -19,8 +21,6 @@ with open('word.txt', 'w') as writer:
             if c not in words_dict:
                 words_dict.add(c)
                 writer.write(f'{c}\n')
-                i+=1
+                i += 1
     writer.write('above\nbelow\nsub\nsup\nl_sup\ninside\nright')
 print(i)
-
-
